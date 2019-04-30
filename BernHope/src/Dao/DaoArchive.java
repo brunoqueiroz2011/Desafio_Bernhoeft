492a0e0c94231cc0ca6ed1fde02fa6d613ef03b7
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2019-04-30 02:30:31 -0300

    Criação do metódo que valida se o arquivo csv corresponde ao padrão do sistema asana e o metódo que checa se os campos existem

--- BernHope/src/Dao/DaoArchive.java
+++ BernHope/src/Dao/DaoArchive.java
@@ -5,6 +5,13 @@
  */
 package Dao;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Bruno
@@ -14,8 +21,36 @@ public class DaoArchive {
     public DaoArchive() {
     }        
     
-    public void readFirtLineFile(String filePath){
+    public String readFirtLineFile(String filePath) throws IOException{
         //Ler o arquivo
+        BufferedReader reader = null;
+        String line = "";
+        String csvDivider = ",";
+        String result = "";
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+            while ((line = reader.readLine())!= null) {
+                
+                String[] campos = line.split(csvDivider); 
+                if (campos.length > 2) {
+                    result = checkFieldsFile(campos);   
+                }else{
+                    result = "Modelo errado";
+                }    
+                break;
+            }
+        }  catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+        
+        return result;
+        
     }
     
     public void readFile(String filePath){
@@ -36,6 +71,38 @@ public class DaoArchive {
     
     public void totalActivities(){
         //Devolve a quantidade total de atividades cadastrada para o aluno
+    }
+
+    private String checkFieldsFile(String [] campos) {
+        String result = "";        
+        if (campos[0].equals("Task ID")){
+            result = "OK";
+        }if (campos[1].equals("Created At")){
+            result = "OK";
+        }if (campos[2].equals("Completed At")){
+            result = "OK";
+        }if (campos[3].equals("Last Modified")){
+            result = "OK";
+        }if (campos[4].equals("Name")){
+            result = "OK";
+        }if (campos[5].equals("Assignee")){
+            result = "OK";
+        }if (campos[6].equals("Assignee Email")){
+            result = "OK";
+        }if (campos[7].equals("Start Date")){
+            result = "OK";
+        }if (campos[8].equals("Due Date")){
+            result = "OK";
+        }if (campos[9].equals("Tags")){
+            result = "OK";
+        }if (campos[10].equals("Notes")){
+            result = "OK";
+        }if (campos[11].equals("Projects")){
+            result = "OK";
+        }if (campos[12].equals("Parent Task")) {
+            result = "OK";
+        }
+        return result;
     }
     
 }
