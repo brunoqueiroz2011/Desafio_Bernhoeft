352abd5ffd6ce39056265175f80975e61394b61b
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2019-04-30 00:06:38 -0300

    Class que fará todas as operações no arquivo csv

---
+++ BernHope/src/Dao/DaoArchive.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+/**
+ *
+ * @author Bruno
+ */
+public class DaoArchive {
+
+    public DaoArchive() {
+    }        
+    
+    public void readFirtLineFile(String filePath){
+        //Ler o arquivo
+    }
+    
+    public void readFile(String filePath){
+        //Ler o arquivo
+    }
+    
+    public void treatFile(){
+        //Adiciona os dados do arquivo no array de alunos
+    }
+    
+    public void quantityActivitiesDone(){
+        //Devolve a quantidade total de atividades realizadas
+    }
+    
+    public void amountOverdueActivities(){
+        //Devolve a quantidade total de atividades atrasadas
+    }
+    
+    public void totalActivities(){
+        //Devolve a quantidade total de atividades cadastrada para o aluno
+    }
+    
+}
