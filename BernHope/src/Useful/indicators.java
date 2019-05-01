eb61ff2d974875ecc5f5c2999f5ec3d793797c5f
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2019-05-01 03:17:24 -0300

    Class que conterar todos os indicadores e  seus códigos

---
+++ BernHope/src/Useful/indicators.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Useful;
+
+/**
+ *
+ * @author Bruno
+ */
+public final class indicators {
+    
+    public static final int INDICADOR_ID_ACOES_ATRASADAS = 3629;
+    public static final String INDICADOR_ACOES_ATRASADAS = "% Ações atrasadas";
+    public static final int INDICADOR_ID_CUMPRIMENTO_TREINAMENTO_JEDI = 3628;
+    public static final String INDICADOR_CUMPRIMENTO_TREINAMENTO_JEDI = "Cumprimento do Treinamento JEDI";
+}
