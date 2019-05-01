ed4edf76775b10e5ef5c4ee8a61fefd02f4e14fa
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2019-05-01 03:16:16 -0300

    Refatoração da class para adicionar os gets e sets

--- BernHope/src/Models/AsanaCSV.java
+++ BernHope/src/Models/AsanaCSV.java
@@ -24,5 +24,130 @@ public class AsanaCSV {
     private String projects;
     private String parentTask;
 
+    public AsanaCSV() {
+    }
+
+    public AsanaCSV(String taskId, String createdAt, String completedAt, String lastModified, String name, String assignee, String assigneeEmail, String startDate, String dueDate, String tags, String notes, String projects, String parentTask) {
+        this.taskId = taskId;
+        this.createdAt = createdAt;
+        this.completedAt = completedAt;
+        this.lastModified = lastModified;
+        this.name = name;
+        this.assignee = assignee;
+        this.assigneeEmail = assigneeEmail;
+        this.startDate = startDate;
+        this.dueDate = dueDate;
+        this.tags = tags;
+        this.notes = notes;
+        this.projects = projects;
+        this.parentTask = parentTask;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(String completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    public String getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(String lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getAssigneeEmail() {
+        return assigneeEmail;
+    }
+
+    public void setAssigneeEmail(String assigneeEmail) {
+        this.assigneeEmail = assigneeEmail;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getProjects() {
+        return projects;
+    }
+
+    public void setProjects(String projects) {
+        this.projects = projects;
+    }
+
+    public String getParentTask() {
+        return parentTask;
+    }
+
+    public void setParentTask(String parentTask) {
+        this.parentTask = parentTask;
+    }
+    
+    
+
 
 }
